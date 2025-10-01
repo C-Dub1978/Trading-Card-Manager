e9dddf72702faa14417f83e7ef3f0eb4453ec219
C-Dub1978 <chris@andersonopt.com>
2025-09-30 23:54:57 -0500

    init repo, which isnt clear

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,7 +95,7 @@ export default function Landing() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to nextjs.org →→→→
         </a>
       </footer>
     </div>
